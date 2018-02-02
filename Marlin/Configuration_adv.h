b78e62fe19dd4b65e936d512a26d79f70a7e4ff8
timotl <34389636+timotl@users.noreply.github.com>
2018-02-02 10:13:19 -0700

    Change thermal protection to match stock firmware

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -87,8 +87,8 @@
  * Thermal Protection parameters for the bed are just as above for hotends.
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
-  #define THERMAL_PROTECTION_BED_PERIOD 20    // Seconds
-  #define THERMAL_PROTECTION_BED_HYSTERESIS 2 // Degrees Celsius
+  #define THERMAL_PROTECTION_BED_PERIOD 90    // Seconds
+  #define THERMAL_PROTECTION_BED_HYSTERESIS 12 // Degrees Celsius
 
   /**
    * As described above, except for the bed (M140/M190/M303).
